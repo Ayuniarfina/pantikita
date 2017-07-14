7a52330a4c2ba5e675f27a1e229227ca93a1a1a2
nblskr <nabila.s.ramadhanti@gmail.com>
2017-07-14 14:47:13 +0700

    route

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -6,8 +6,8 @@
   <h2 class="text-50">Ayo lestarikan panti</h2>
   <h2 class="text-50">Untuk masa depan bangsa</h2>
   <br>
-  <h3>“Sesungguhnya tidak akan berkurang harta yang disedekahkan, kecuali bertambah dan bertambah”</h3>
-  <h4>(HR. Tirmidzi)</h4>
+  <h3></h3>
+  <h4></h4>
   <br><br>
   <p><a href="../register" class="btn btn-primary btn-lg">Gabung</a></p>
 </div>
@@ -18,8 +18,8 @@
 <div class="container">
 
 <blockquote>
-  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante. khiuril iulrsilji lklji io99uoi wwwiuworbashuidk uhkjhih hiuhikjh hjihhuiuh</p>
-  <small>Someone famous in <cite title="Source Title">Source Title</cite></small>
+  <p>“Sesungguhnya tidak akan berkurang harta yang disedekahkan, kecuali bertambah dan bertambah”</p>
+  <small>HR.<cite title="Source Title"> Tirmidzi</cite></small>
 </blockquote>
 <br>
 <div class="container text-center">  
@@ -29,7 +29,7 @@
       <h3 class="pull-left">Panti Terdaftar </h3>
     </div>
     <div>
-    <a href="../regispanti" class="btn btn-primary pull-right">
+    <a href="../userPanti/create" class="btn btn-primary pull-right">
       <h4>Daftarkan Panti</h4></a>
     </div>
   </div>
